bc353d15e85e51c610583117b434f23ba35b0186
Winter-Briskfire <hansend6839@gmail.com>
2025-12-12 18:14:42 -0700

    For the final project.

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,44 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address = new Address("789 Center St", "Denver", "CO", "USA");
+
+        Event lecture = new Lecture(
+            "Tech Talk",
+            "Future of AI",
+            "April 10, 2025",
+            "6:00 PM",
+            address,
+            "Dr. Smith",
+            200);
+
+        Event reception = new Reception(
+            "Company Mixer",
+            "Networking event",
+            "May 5, 2025",
+            "7:00 PM",
+            address,
+            "rsvp@company.com");
+
+        Event outdoor = new OutdoorGathering(
+            "Community Picnic",
+            "Family-friendly event",
+            "June 1, 2025",
+            "12:00 PM",
+            address,
+            "Sunny");
+
+        List<Event> events = new List<Event> { lecture, reception, outdoor };
+
+        foreach (Event e in events)
+        {
+            Console.WriteLine(e.GetStandardDetails());
+            Console.WriteLine();
+            Console.WriteLine(e.GetFullDetails());
+            Console.WriteLine();
+            Console.WriteLine(e.GetShortDescription());
+            Console.WriteLine("\n------------------\n");
+        }
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 3.0),
+            new Cycling("03 Nov 2022", 45, 15.0),
+            new Swimming("03 Nov 2022", 40, 20)
+        };
+
+        foreach (Activity a in activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
